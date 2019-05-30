28dd24c75020b11119771b3e72b60123151ce593
Milind Mevada <milind.mevada@solutelabs.com>
2019-05-30 14:32:04 +0530

    Separated logic and data layer.

--- lib/blocs/github_repo_bloc.dart
+++ lib/blocs/github_repo_bloc.dart
@@ -1,47 +1,22 @@
 import 'package:graph_ql_poc/models/github_repo_model.dart';
 import 'package:graph_ql_poc/models/view_model/github_repo_model.dart';
-import 'package:graph_ql_poc/network_utils/graphql_client.dart';
-import 'package:graphql/client.dart';
+import 'package:graph_ql_poc/providers/github_repo_provider.dart';
 import 'package:intl/intl.dart';
 import 'package:rxdart/rxdart.dart';
 
 class GitHubRepoBLoC {
   final repoList = BehaviorSubject<List<GitHubRepoModel>>();
+  final _gitHubRepo = GitHubRepoProvider();
 
   GitHubRepoBLoC() {
     _getRepos()
-        .then(toGitHubRepo)
         .then(toViewModel)
         .then((list) => repoList.value = list)
         .catchError((err) => print('Error getting repo $err'));
   }
 
-  Future<QueryResult> _getRepos() {
-    return getGraphQLClient().query(_queryOptions());
-  }
-
-  QueryOptions _queryOptions() {
-    return QueryOptions(
-      document: readRepositories,
-      variables: <String, dynamic>{
-        'nRepositories': 50,
-      },
-    );
-  }
-
-  List<GithubRepo> toGitHubRepo(QueryResult queryResult) {
-    if (queryResult.hasErrors) {
-      throw Exception();
-    }
-
-    final list =
-        queryResult.data['viewer']['repositories']['nodes'] as List<dynamic>;
-
-    print('Data from API: $list');
-
-    return list
-        .map((repoJson) => GithubRepo.fromJson(repoJson))
-        .toList(growable: false);
+  Future<List<GithubRepo>> _getRepos() {
+    return _gitHubRepo.getCurrentUserRepos();
   }
 
   List<GitHubRepoModel> toViewModel(List<GithubRepo> dataModelList) {
@@ -63,17 +38,3 @@ class GitHubRepoBLoC {
     repoList.close();
   }
 }
-
-const String readRepositories = r'''
-  query ReadRepositories($nRepositories: Int!) {
-    viewer {
-      repositories(last: $nRepositories) {
-        nodes {
-          name
-          createdAt
-          forkCount
-        }
-      }
-    }
-  }
-''';
---
+++ lib/providers/github_repo_provider.dart
@@ -0,0 +1,45 @@
+import 'package:graph_ql_poc/models/github_repo_model.dart';
+import 'package:graph_ql_poc/network_utils/graphql_client.dart';
+import 'package:graphql/client.dart';
+
+class GitHubRepoProvider {
+  Future<List<GithubRepo>> getCurrentUserRepos() {
+    return getGraphQLClient().query(_queryOptions()).then(_toGitHubRepo);
+  }
+
+  QueryOptions _queryOptions() {
+    return QueryOptions(
+      document: readRepositories,
+      variables: <String, dynamic>{
+        'nRepositories': 50,
+      },
+    );
+  }
+
+  List<GithubRepo> _toGitHubRepo(QueryResult queryResult) {
+    if (queryResult.hasErrors) {
+      throw Exception();
+    }
+
+    final list =
+        queryResult.data['viewer']['repositories']['nodes'] as List<dynamic>;
+
+    return list
+        .map((repoJson) => GithubRepo.fromJson(repoJson))
+        .toList(growable: false);
+  }
+}
+
+const String readRepositories = r'''
+  query ReadRepositories($nRepositories: Int!) {
+    viewer {
+      repositories(last: $nRepositories) {
+        nodes {
+          name
+          createdAt
+          forkCount
+        }
+      }
+    }
+  }
+''';
